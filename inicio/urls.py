2bdffeb669ec41c3d4acb60f5def1ac615198f57
fagustoni <fjaguston@gmail.com>
2024-06-10 19:17:49 -0300

    vuelta a cambiar

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from inicio import views
 
 urlpatterns = [
-    path("",views.inicio,name ="inicio"),
+    path("",views.inicio,"inicio"),
     path("template1/<str:nombre>/<str:apellido>/<int:edad>",views.template1),
     path("template4/<str:nombre>/<str:apellido>/<int:edad>",views.template4),
     path("prueba/",views.probando, name= "probando"),
     path("autos/crear/<str:marca>/<str:modelo>/",views.crear_auto),
+    
 ]    
    
 
